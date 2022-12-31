92a25abb1cc019e23230922946d7435db45a5325
Anton B <antonbsa.bck@gmail.com>
2022-12-31 17:14:25 -0300

    add checkbox to disable/enable button - and the test

--- color-button/src/App.js
+++ color-button/src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 
 function App() {
   const [isRed, setIsRed] = useState(true);
+  const [isDisable, setIsDisable] = useState(false);
 
   return (
     <div>
       <button style={{ backgroundColor: isRed ? 'red' : 'dodgerblue' }}
         onClick={() => setIsRed(!isRed)}
+        disabled={isDisable}
       >
         Change to {isRed ? 'blue' : 'red'}
       </button>
+      <br />
+      <span>{isDisable ? 'Enable' : 'Disable'} button</span>
+      <input type="checkbox" onClick={() => setIsDisable(!isDisable)} />
     </div>
   );
 }
--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -11,3 +11,25 @@ test('button has correct initial color and updates when clicked', () => {
   expect(colorButton).toHaveStyle({ backgroundColor: 'dodgerblue' });
   expect(colorButton).toHaveTextContent('Change to red');
 });
+
+test('button disabled when the checkbox is checked', () => {
+  render(<App />);
+
+  // Initial state
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  const checkbox = screen.getByRole('checkbox');
+  expect(colorButton).toBeEnabled();
+  expect(screen.getByText('Disable button')).toBeInTheDocument();
+
+  // Clicking checkbox disables button
+  fireEvent.click(checkbox);
+  expect(checkbox).toBeChecked();
+  expect(colorButton).toBeDisabled();
+  expect(screen.getByText('Enable button')).toBeInTheDocument();
+
+  // Clicking checkbox again enables button
+  fireEvent.click(checkbox);
+  expect(checkbox).not.toBeChecked();
+  expect(colorButton).toBeEnabled();
+  expect(screen.getByText('Disable button')).toBeInTheDocument();
+})
